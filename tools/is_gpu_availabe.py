a8f1a7673008dc98ab340d0fa37b19050f664d4b
Uplink <olsj21@student.bth.se>
2026-03-06 08:28:13 +0000

    fix: Add an l to is gpu available

--- tools/is_gpu_availabe.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-if __name__ == "__main__":
-    if torch.cuda.is_available():
-        print("CUDA is available. GPU can be used.")
-    else:
-        print("CUDA is not available. GPU cannot be used.")
